1003e6bbcfd5edf32b6eb2324700f0474345c7e5
Muhammed Lasin <lasinluay1@gmail.com>
2022-07-26 12:30:47 +0530

    Final file

---
+++ src/Components/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+import CreateArea from "./CreateArea";
+
+
+function App(){
+
+    const [notes, setNotes] = useState([])
+
+    function onAdd(input) {
+        setNotes((prevValue)=>{
+            return [...prevValue, input]
+        })
+        console.log(notes);
+    }
+    
+    function onDelete(id){
+            setNotes((prevLists)=>{
+                return prevLists.filter((list,index)=>{
+                    return index!= id;
+                })
+            })
+    }
+
+    
+    return <div>
+    <Header/>
+    <CreateArea add={onAdd}/>
+    {notes.map((note,index)=>{
+        return <Note delete={onDelete}  key={index} title={note.title} id={index} content={note.content} />
+    })}
+    
+    <Footer/>
+    </div>;
+}
+export default App;
--- src/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function Footer(){
-    var date = new Date().getFullYear();
-    return <footer><p>Copyright {date}</p></footer>;
-}
-export default Footer;
