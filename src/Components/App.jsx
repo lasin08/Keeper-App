4d0e9ff34bbe116924ffa0e1b6c7dd60c2011063
Muhammed Lasin <lasinluay1@gmail.com>
2022-09-04 18:07:25 +0530

    new changes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,35 +4,41 @@ import Note from "./Note";
 import Footer from "./Footer";
 import CreateArea from "./CreateArea";
 
+function App() {
+  const [notes, setNotes] = useState([]);
 
-function App(){
+  function onAdd(input) {
+    setNotes((prevValue) => {
+      return [...prevValue, input];
+    });
+    console.log(notes);
+  }
 
-    const [notes, setNotes] = useState([])
+  function onDelete(id) {
+    setNotes((prevLists) => {
+      return prevLists.filter((list, index) => {
+        return index != id;
+      });
+    });
+  }
 
-    function onAdd(input) {
-        setNotes((prevValue)=>{
-            return [...prevValue, input]
-        })
-        console.log(notes);
-    }
-    
-    function onDelete(id){
-            setNotes((prevLists)=>{
-                return prevLists.filter((list,index)=>{
-                    return index!= id;
-                })
-            })
-    }
-
-    
-    return <div>
-    <Header/>
-    <CreateArea add={onAdd}/>
-    {notes.map((note,index)=>{
-        return <Note delete={onDelete}  key={index} title={note.title} id={index} content={note.content} />
-    })}
-    
-    <Footer/>
-    </div>;
+  return (
+    <div>
+      <Header />
+      <CreateArea add={onAdd} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            delete={onDelete}
+            key={index}
+            title={note.title}
+            id={index}
+            content={note.content}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 export default App;
--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,53 +1,64 @@
-import React, { useState } from 'react'
-import AddIcon from '@mui/icons-material/Add';
-import Fab from '@mui/material/Fab';
-import Zoom from '@mui/material/Zoom';
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
-  
   const [input, setInput] = useState({
-    title:"",
-    content:""
-  })  
+    title: "",
+    content: "",
+  });
 
-  const [isTrue, setIsTrue] = useState(false)
+  const [isTrue, setIsTrue] = useState(false);
 
   function handleChange(event) {
-
-    const {name, value} = event.target;
-    setInput((prevValue)=>{
-        return ({...prevValue, [name]:value});
-    })
+    const { name, value } = event.target;
+    setInput((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
     console.log(input);
-
-  } 
+  }
 
   function handleClick(event) {
     event.preventDefault();
-    props.add(input)
+    props.add(input);
     setInput({
-        title:"",
-        content:""
-    })
+      title: "",
+      content: "",
+    });
   }
-  
-  function handleDrop(){
-        setIsTrue(true)
+
+  function handleDrop() {
+    setIsTrue(true);
   }
-  
-return (
-    <div> 
-    <form className='create-note'>
-    {
-        isTrue && <input name="title" value={input.title} placeholder="Title" onChange={handleChange} />
-     }
-     <textarea name="content" onClick={handleDrop} value={input.content} placeholder="Take a note..." rows={isTrue ? 3 : 1} onChange={handleChange} />
-    <Zoom in={isTrue}>
-    <Fab onClick={handleClick} ><AddIcon/></Fab>
-    </Zoom>
-    </form>
-  </div>
-  )
+
+  return (
+    <div>
+      <form className="create-note">
+        {isTrue && (
+          <input
+            name="title"
+            value={input.title}
+            placeholder="Title"
+            onChange={handleChange}
+          />
+        )}
+        <textarea
+          name="content"
+          onClick={handleDrop}
+          value={input.content}
+          placeholder="Take a note..."
+          rows={isTrue ? 3 : 1}
+          onChange={handleChange}
+        />
+        <Zoom in={isTrue}>
+          <Fab onClick={handleClick}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
 }
 
 export default CreateArea;
